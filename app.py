d6c72c44fa315426eae4c296176cef545296b3c4
iiihenrymorrin <henry.morrin@iii.com>
2018-08-24 14:22:46 +0100

    The first commit on Student App

---
+++ app.py
@@ -0,0 +1,12 @@
+#Udemy Coursework -> Create student registration system
+
+
+
+
+
+
+def create_student():
+    #Ask the user for students name
+    #Create the dictionary
+    #Return that dictionary
+    name = str(raw_input("Please enter the students name: "))
