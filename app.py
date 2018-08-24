17a57b42d4851fa753067934b6f3aa272bfca053
iiihenrymorrin <henry.morrin@iii.com>
2018-08-24 16:11:21 +0100

    Added the function to calculate teh average mark

--- app.py
+++ app.py
@@ -11,8 +11,26 @@ def create_student():
                     }
     return student_data
 
+def add_mark(student,mark):
+    #append mark to student dictionary
+    student["marks"].append(mark)
+    return None
+
+def calculate_average_mark(student):
+    #Add together all of the students[marks]
+    #Divide them by the total number of marks
+    #What happens if the student has no marks
+    try:
+        total = sum(student["marks"])
+        no_of_marks = len(student["marks"])
+        average = total / no_of_marks
+        return average
+    except:
+        print("Error encounter - stopping program")
 
 
-student1_json=create_student()
-student1_json["marks"].append(20)
-print(student1_json)
+s=create_student()
+add_mark(s,90)
+add_mark(s,50)
+print(s)
+print("{} average mark is {}".format(s["name"],calculate_average_mark(s)))
