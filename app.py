04ca4c9c22b9d2742b4026a1657e88f423293e56
iiihenrymorrin <henry.morrin@iii.com>
2018-08-24 15:02:32 +0100

    Added code to append to list in dictionary

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 #Udemy Coursework -> Create student registration system
 
 
-
-
-
-
 def create_student():
     #Ask the user for students name
     #Create the dictionary
     #Return that dictionary
     name = str(raw_input("Please enter the students name: "))
+    student_data={ "name" : name,
+                    "marks": []
+                    }
+    return student_data
+
+
+
+student1_json=create_student()
+student1_json["marks"].append(20)
+print(student1_json)
